760a96368e3ac5551d41ead72614dd62941cec4b
wjsxhclj <wjsxhclj@sina.com>
2019-02-26 15:48:18 +0800

    Demo Ctrl.js没有了alert

--- front/business/home/homeCtrl.js
+++ front/business/home/homeCtrl.js
@@ -1,7 +1,7 @@
 define(['app', 'Service'], function (app) {
     app.register
-        .controller('homeCtrl', ['$http', '$scope', '$location', '$cookies', '$rootScope', 'Alert', 'BEWG_URL',
-            function ($http, $scope, $location, $cookies, $rootScope, Alert, BEWG_URL) {
+        .controller('homeCtrl', ['$http', '$scope', '$location', '$cookies', '$rootScope', 'Window', 'BEWG_URL',
+            function ($http, $scope, $location, $cookies, $rootScope, Window, BEWG_URL) {
                 // Alert.alert("aaaaaaa");
                 console.log(BEWG_URL.port);
                 console.log("这是首页");
