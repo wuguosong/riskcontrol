eade679b86da6e0b4e08c2ffd621136fa4cc0be9
wjsxhclj@sina.com <wjsxhclj@sina.com>
2019-04-09 09:56:13 +0800

    分享页面样式修改

--- rcm_3.0_modify/html/page/sys/common/message_share.js
+++ rcm_3.0_modify/html/page/sys/common/message_share.js
@@ -6,7 +6,7 @@ ctmApp.register.controller('shareMessageCtrl', ['$http', '$scope', '$location', 
         $scope.url = $routeParams.url;
         $scope.message = {};
         $scope._messages_array_ = [];
-        $scope.queryMessagesList = function (procInstId, parentId) {
+        $scope._query_messages_list_ = function (procInstId, parentId) {
             $http({
                 method: 'post',
                 url: srvUrl + 'message/queryMessagesList.do',
@@ -19,6 +19,27 @@ ctmApp.register.controller('shareMessageCtrl', ['$http', '$scope', '$location', 
                 $scope._messages_array_ = data;
             });
         };
+        // 用户的其他操作,预留方法
+        $scope._user_about_click_ = function(_source_, _user_id_, _user_name_){
+            console.log("查看用户：" + _source_ + " " + _user_id_ + " " + _user_name_);
+        };
+        // 检测滑动
+        $scope._check_collapse_event_ = function(_obj_, _idx_){
+            var _class = $('#_message_panel_body_' + _idx_).attr("class");
+            var _span = '';
+            if("panel-collapse collapse in" == _class){
+                _span += _obj_['createdName'] + '&nbsp;';
+                _span += _obj_['messageDate'] + '&nbsp;';
+                if(!isEmpty(_obj_.messageContent)){
+                    if(_obj_.messageContent.length >= 10){
+                        _span += _obj_.messageContent.substr(0, 11) + '......';
+                    }else{
+                        _span += _obj_.messageContent;
+                    }
+                }
+            }
+            $('#_span_hide_content_' + _idx_).html(_span);
+        };
         $scope.initMessage = function(messageId){
             $http({
                 method: 'post',
@@ -29,7 +50,9 @@ ctmApp.register.controller('shareMessageCtrl', ['$http', '$scope', '$location', 
                 headers: {'Content-Type': 'application/x-www-form-urlencoded'}
             }).success(function (data) {
                 $scope.message = data['result_data'];
-                $scope.queryMessagesList($scope.message.procInstId, 0);
+                if(!isEmpty($scope.message)){
+                    $scope._query_messages_list_($scope.message.procInstId, 0);
+                }
             });
         };
         $scope.initMessage($routeParams.id);
