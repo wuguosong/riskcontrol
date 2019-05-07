e4f1050ac07ace6c7e29e32b952fce18968413d0
wjsxhclj@sina.com <wjsxhclj@sina.com>
2019-05-07 11:28:48 +0800

    其它评审：查看页面将相关资源和留言区进行了整合。

--- rcm_3.0_modify/html/page/rcm/bulletinMatters/BulletinMatters.js
+++ rcm_3.0_modify/html/page/rcm/bulletinMatters/BulletinMatters.js
@@ -691,4 +691,9 @@ ctmApp.register.controller('BulletinMattersDetailView', ['$http','$scope','$loca
 			$(":checkbox[name='choose']").attr("checked",false);
 		}
 	}
+     $scope._init_uuid_ = $scope.credentials.UUID;
+     $scope._init_messages_array_ = _init_query_messages_list_($routeParams.id);
+     ////////////审批阶段对留言编辑权限的控制
+     var curTask = wf_getCurrentTask('bulletin', $routeParams.id);
+     $scope._message_publish_reply_ = !isEmpty(curTask) && curTask.TASK_DEF_KEY_ != 'usertask7';
 }]);
