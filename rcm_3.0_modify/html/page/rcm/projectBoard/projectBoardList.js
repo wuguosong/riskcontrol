a57b9337e81862993c6367edf2a465a734e6d351
qirui <qirui@promisects.com>
2019-06-04 19:54:18 +0800

    修改项目看板分页

--- rcm_3.0_modify/html/page/rcm/projectBoard/projectBoardList.js
+++ rcm_3.0_modify/html/page/rcm/projectBoard/projectBoardList.js
@@ -5,85 +5,7 @@ ctmApp.register.controller('projectBoardList', ['$routeParams','$http','$scope',
     $scope.oldUrl = $routeParams.url;
     $scope.tabIndex = '0';
 
-    $scope.initData = function(){
-        $scope.getProjectList1();
-        $scope.getProjectList2();
-        $scope.getProjectList3();
-    };
-    // 查询项目看板分配任务及之前的项目
-    $scope.getProjectList1 = function(){
-        if($scope.paginationConf.queryObj == null || $scope.paginationConf.queryObj == ''){
-            $scope.paginationConf.queryObj = {};
-        }
-
-        if ($scope.projectName != null && $scope.projectName != ''){
-            var projectName = $filter('decodeURI')($scope.projectName,"VALUE");
-            $scope.paginationConf.queryObj.projectName = projectName;
-        }
-        $scope.paginationConf.queryObj.userId = $scope.credentials.UUID;
-        $scope.paginationConf.queryObj.stageList = ['1', '2'];
-        $http({
-            method:'post',
-            url: srvUrl + "projectBoard/getProjectList.do",
-            data: $.param({
-                "page":JSON.stringify($scope.paginationConf),
-                "json":JSON.stringify($scope.paginationConf.queryObj)
-            })
-        }).success(function(result){
-            $scope.projectList1 = result.result_data.list;
-            $scope.paginationConf.totalItems = result.result_data.totalItems;
-        });
-    };
-    // 查询项目看板风控任务的项目
-    $scope.getProjectList2 = function(){
-        if($scope.paginationConfes.queryObj == null || $scope.paginationConf.queryObj == ''){
-            $scope.paginationConfes.queryObj = {};
-        }
-
-        if ($scope.projectName != null && $scope.projectName != ''){
-            var projectName = $filter('decodeURI')($scope.projectName,"VALUE");
-            $scope.paginationConfes.queryObj.projectName = projectName;
-        }
-        $scope.paginationConfes.queryObj.userId = $scope.credentials.UUID;
-        $scope.paginationConfes.queryObj.stageList = ['3','4','5','6'];
-        $http({
-            method:'post',
-            url: srvUrl + "projectBoard/getProjectList.do",
-            data: $.param({
-                "page":JSON.stringify($scope.paginationConfes),
-                "json":JSON.stringify($scope.paginationConfes.queryObj)
-            })
-        }).success(function(result){
-            $scope.projectList2 = result.result_data.list;
-            $scope.paginationConfes.totalItems = result.result_data.totalItems;
-        });
-    };
-    // 查询项目看板已完成的项目
-    $scope.getProjectList3 = function(){
-        if($scope.paginationConfesd.queryObj == null || $scope.paginationConfesd.queryObj == ''){
-            $scope.paginationConfesd.queryObj = {};
-        }
-
-        if ($scope.projectName != null && $scope.projectName != ''){
-            var projectName = $filter('decodeURI')($scope.projectName,"VALUE");
-            $scope.paginationConfesd.queryObj.projectName = projectName;
-        }
-        $scope.paginationConfesd.queryObj.userId = $scope.credentials.UUID;
-        $scope.paginationConfesd.queryObj.stageList = ['7','9'];
-        $http({
-            method:'post',
-            url: srvUrl + "projectBoard/getProjectList.do",
-            data: $.param({
-                "page":JSON.stringify($scope.paginationConfesd),
-                "json":JSON.stringify($scope.paginationConfesd.queryObj)
-            })
-        }).success(function(result){
-            $scope.projectList3 = result.result_data.list;
-            $scope.paginationConfesd.totalItems = result.result_data.totalItems;
-        });
-    };
-
-    $scope.paginationConf = {
+    $scope.paginationConf1 = {
         currentPage: 1,
         totalItems: 0,
         itemsPerPage: 10,
@@ -91,7 +13,7 @@ ctmApp.register.controller('projectBoardList', ['$routeParams','$http','$scope',
         perPageOptions: [10, 20, 30, 40, 50],
         queryObj: {}
     };
-    $scope.paginationConfes = {
+    $scope.paginationConf2 = {
         currentPage: 1,
         totalItems: 0,
         itemsPerPage: 10,
@@ -101,7 +23,7 @@ ctmApp.register.controller('projectBoardList', ['$routeParams','$http','$scope',
         onChange: function () {
         }
     };
-    $scope.paginationConfesd = {
+    $scope.paginationConf3 = {
         currentPage: 1,
         totalItems: 0,
         itemsPerPage: 10,
@@ -112,10 +34,87 @@ ctmApp.register.controller('projectBoardList', ['$routeParams','$http','$scope',
         }
     };
 
+    // 查询项目看板分配任务及之前的项目
+    $scope.getProjectList1 = function(){
+        console.log("sssssssssssssss");
+        if($scope.paginationConf1.queryObj == null || $scope.paginationConf1.queryObj == ''){
+            $scope.paginationConf1.queryObj = {};
+        }
+
+        if ($scope.projectName != null && $scope.projectName != ''){
+            var projectName = $filter('decodeURI')($scope.projectName,"VALUE");
+            $scope.paginationConf1.queryObj.projectName = projectName;
+        }
+        $scope.paginationConf1.queryObj.userId = $scope.credentials.UUID;
+        $scope.paginationConf1.queryObj.stageList = ['1', '2'];
+        $http({
+            method:'post',
+            url: srvUrl + "projectBoard/getProjectList.do",
+            data: $.param({
+                "page":JSON.stringify($scope.paginationConf1),
+                "json":JSON.stringify($scope.paginationConf1.queryObj)
+            })
+        }).success(function(result){
+            $scope.projectList1 = result.result_data.list;
+            $scope.paginationConf1.totalItems = result.result_data.totalItems;
+        });
+    };
+    // 查询项目看板风控任务的项目
+    $scope.getProjectList2 = function(){
+        if($scope.paginationConf2.queryObj == null || $scope.paginationConf2.queryObj == ''){
+            $scope.paginationConf2.queryObj = {};
+        }
+
+        if ($scope.projectName != null && $scope.projectName != ''){
+            var projectName = $filter('decodeURI')($scope.projectName,"VALUE");
+            $scope.paginationConf2.queryObj.projectName = projectName;
+        }
+        $scope.paginationConf2.queryObj.userId = $scope.credentials.UUID;
+        $scope.paginationConf2.queryObj.stageList = ['3','4','5','6'];
+        $http({
+            method:'post',
+            url: srvUrl + "projectBoard/getProjectList.do",
+            data: $.param({
+                "page":JSON.stringify($scope.paginationConf2),
+                "json":JSON.stringify($scope.paginationConf2.queryObj)
+            })
+        }).success(function(result){
+            $scope.projectList2 = result.result_data.list;
+            $scope.paginationConf2.totalItems = result.result_data.totalItems;
+        });
+    };
+    // 查询项目看板已完成的项目
+    $scope.getProjectList3 = function(){
+        if($scope.paginationConf3.queryObj == null || $scope.paginationConf3.queryObj == ''){
+            $scope.paginationConf3.queryObj = {};
+        }
+
+        if ($scope.projectName != null && $scope.projectName != ''){
+            var projectName = $filter('decodeURI')($scope.projectName,"VALUE");
+            $scope.paginationConf3.queryObj.projectName = projectName;
+        }
+        $scope.paginationConf3.queryObj.userId = $scope.credentials.UUID;
+        $scope.paginationConf3.queryObj.stageList = ['7','9'];
+        $http({
+            method:'post',
+            url: srvUrl + "projectBoard/getProjectList.do",
+            data: $.param({
+                "page":JSON.stringify($scope.paginationConf3),
+                "json":JSON.stringify($scope.paginationConf3.queryObj)
+            })
+        }).success(function(result){
+            $scope.projectList3 = result.result_data.list;
+            $scope.paginationConf3.totalItems = result.result_data.totalItems;
+        });
+    };
+
+
+
     // 通过$watch currentPage和itemperPage 当他们一变化的时候，重新获取数据条目
-    $scope.$watch('paginationConf.currentPage + paginationConf.itemsPerPage', $scope.getProjectList1());
+    $scope.$watch('paginationConf1.currentPage + paginationConf1.itemsPerPage', $scope.getProjectList1);
     // 通过$watch currentPage和itemperPage 当他们一变化的时候，重新获取数据条目
-    $scope.$watch('paginationConfes.currentPage + paginationConfes.itemsPerPage', $scope.getProjectList2());
+    $scope.$watch('paginationConf2.currentPage + paginationConf2.itemsPerPage', $scope.getProjectList2);
     // 通过$watch currentPage和itemperPage 当他们一变化的时候，重新获取数据条目
-    $scope.$watch('paginationConfesd.currentPage + paginationConfesd.itemsPerPage', $scope.getProjectList3());
+    $scope.$watch('paginationConf3.currentPage + paginationConf3.itemsPerPage', $scope.getProjectList3);
+
 }]);
