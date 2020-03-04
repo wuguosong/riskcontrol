e5fb08fa2ec8eb6ed4641981ecfd007c0dc356d3
misssansui <493473498@qq.com>
2020-03-04 23:10:28 +0800

    删除无用代码

--- front/business/home/homeCtrl.js
+++
@@ -1,9 +0,0 @@
-define(['app', 'Service'], function (app) {
-    app.register
-        .controller('homeCtrl', ['$http', '$scope', '$location', '$cookies', '$rootScope', 'Window', 'BEWG_URL',
-            function ($http, $scope, $location, $cookies, $rootScope, Window, BEWG_URL) {
-                // Alert.alert("aaaaaaa");
-                console.log(BEWG_URL.port);
-                console.log("这是首页");
-            }])
-});
--- front/business/report/reportCtrl.js
+++
@@ -1,7 +0,0 @@
-﻿define(['app'], function (app) {
-    app
-        .register.controller('reportCtrl', ['$http', '$scope', '$location', '$rootScope',
-        function ($http, $scope, $location, $rootScope) {
-            console.log("报表");
-        }]);
-});
